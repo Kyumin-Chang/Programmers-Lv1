e0e7dab3b72d32a6c65798f3fab86096785d466e
Kyumin-Chang <smil0529@naver.com>
2023-11-21 21:07:08 +0900

    feat: Add RunningRace Problem

---
+++ src/main/java/RunningRace.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class RunningRace {
+    private final String[] players = {"mumu", "soe", "poe", "kai", "mine"};
+    private final String[] callings = {"kai", "kai", "mine", "mine"};
+
+    public String[] getPlayers(){
+        return players;
+    }
+
+    public String[] getCallings() {
+        return callings;
+    }
+
+    public String[] solution(String[] players, String[] callings) {
+        Map<String, Integer> rankMap = new HashMap<>();
+        for(int i=0; i<players.length; i++) {
+            rankMap.put(players[i], i);
+        }
+        for(String player : callings) {
+            int ownRank = rankMap.get(player);
+            String beforePlayer = players[ownRank-1];
+            players[ownRank-1] = player;
+            players[ownRank] = beforePlayer;
+            rankMap.put(player, ownRank-1);
+            rankMap.put(beforePlayer, ownRank);
+        }
+        return players;
+    }
+}
---
+++ src/test/java/RunningRaceTest.java
@@ -0,0 +1,18 @@
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class RunningRaceTest {
+
+    @DisplayName("달리기경주게임")
+    @Test
+    void runningRaceTest(){
+        RunningRace runningRace = new RunningRace();
+        String[] answer = {"mumu", "kai", "mine", "soe", "poe"};
+        String[] result = runningRace.solution(runningRace.getPlayers(), runningRace.getCallings());
+        System.out.println(Arrays.toString(result));
+        assertThat(result).isEqualTo(answer);
+    }
+}
