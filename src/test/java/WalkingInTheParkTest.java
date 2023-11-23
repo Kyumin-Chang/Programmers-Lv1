847e503e98ae785fe7fc84c2899ba4e131599ec0
Kyumin-Chang <kyumin0529@naver.com>
2023-11-23 16:25:58 +0900

    test: Modify test code

--- src/test/java/WalkingInTheParkTest.java
+++ src/test/java/WalkingInTheParkTest.java
@@ -1,4 +1,11 @@
-public class WalkingInTheParkTest {
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
+public class WalkingInTheParkTest {
+    @DisplayName("")
+    @Test
+    void parkTest() {
+
+    }
 
 }
