c76298eb219007890a8a7e6dc054dabfdd95590d
Kyumin-Chang <kyumin0529@naver.com>
2023-11-24 18:51:28 +0900

    test: Test for WalkingInThePark

--- src/test/java/WalkingInTheParkTest.java
+++ src/test/java/WalkingInTheParkTest.java
@@ -1,11 +1,19 @@
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class WalkingInTheParkTest {
-    @DisplayName("")
+    @DisplayName("공원 산책 문제에 대한 테스트")
     @Test
     void parkTest() {
-
+        WalkingInThePark walkingInThePark = new WalkingInThePark();
+        String[] park = {"SOO", "OOO", "OOO"};
+        String[] routs = {"E 2", "S 2", "W 1"};
+        int[] answer = walkingInThePark.solution(park, routs);
+        System.out.println(Arrays.toString(answer));
+        int[] expectedResults = {2, 1};
+        assertThat(answer).isEqualTo(expectedResults);
     }
-
 }
