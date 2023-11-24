e7e8062e391262fbba3ea2cf674b6eb1b26093e9
Kyumin-Chang <kyumin0529@naver.com>
2023-11-24 22:47:12 +0900

    feat: Solve collectPersonalInformation problem

--- src/main/java/CollectPersonalInformation.java
+++ src/main/java/CollectPersonalInformation.java
@@ -1,19 +1,84 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
 public class CollectPersonalInformation {
     /*
     입력
     today = "2022.05.19"
     terms = ["A 6", "B 12", "C 3"]
-    privacy = ["2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"]
+    privacies = ["2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"]
     출력
     result = [1,3]
      */
 
     public int[] solution(String today, String[] terms, String[] privacies) {
-        int[] answer = {};
-        return answer;
+        return getResults(today, terms, privacies);
     }
 
-    String[] getDateArray(String date) {
+    public int[] getResults(String today, String[] terms, String[] privacies) {
+        String[] todayArray = getDateArray(today);
+        Map<String, Integer> termMap = makeMap(terms);
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < privacies.length; i++) {
+            String[] splitPrivacy = privacies[i].split(" ");
+            String[] addResult = addDate(splitPrivacy[0], termMap.get(splitPrivacy[1]));
+            if (Arrays.equals(todayArray, findBigDate(todayArray, addResult))) {
+                result.add(i + 1);
+            }
+        }
+        return result.stream().mapToInt(i -> i).toArray();
+    }
+
+    public String[] findBigDate(String[] date_1, String[] date_2) {
+        int[] newDate_1 = Stream.of(date_1).mapToInt(Integer::parseInt).toArray();
+        int[] newDate_2 = Stream.of(date_2).mapToInt(Integer::parseInt).toArray();
+        int[] result = newDate_1;
+        if (result[0] < newDate_2[0]) {
+            result = newDate_2;
+        }
+        if ((result[0] == newDate_2[0]) && (result[1] < newDate_2[1])) {
+            result = newDate_2;
+        }
+        if ((result[0] == newDate_2[0]) && (result[1] == newDate_2[1]) && (result[2] < newDate_2[2])) {
+            result = newDate_2;
+        }
+        String[] resultStr = Arrays.stream(result)
+                .mapToObj(String::valueOf)
+                .toArray(String[]::new);
+        resultStr[1] = String.format("%02d", Integer.parseInt(resultStr[1]));
+        resultStr[2] = String.format("%02d", Integer.parseInt(resultStr[2]));
+        return resultStr;
+    }
+
+    public String[] addDate(String date, int month) {
+        String[] dateArray = getDateArray(date);
+        int year = Integer.parseInt(dateArray[0]);
+        int dateMonth = Integer.parseInt(dateArray[1]);
+        dateMonth += month;
+        while (dateMonth > 12) {
+            year++;
+            dateMonth -= 12;
+        }
+        dateArray[0] = String.valueOf(year);
+        dateArray[1] = String.valueOf(dateMonth);
+        return dateArray;
+    }
+
+    public String[] getDateArray(String date) {
         return date.split("\\.");
+    }
+
+    public Map<String, Integer> makeMap(String[] str) {
+        Map<String, Integer> termMap = new HashMap<>();
+        String[] strArray;
+        for (String s : str) {
+            strArray = s.split(" ");
+            termMap.put(strArray[0], Integer.parseInt(strArray[1]));
+        }
+        return termMap;
     }
 }
--- src/test/java/CollectPersonalInformationTest.java
+++ src/test/java/CollectPersonalInformationTest.java
@@ -1,6 +1,7 @@
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Arrays;
+import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -16,5 +17,46 @@ public class CollectPersonalInformationTest {
         assertThat(result).isEqualTo(answer);
     }
 
-    
+    @DisplayName("날짜 비교 테스트")
+    @Test
+    void compareDateTest() {
+        String[] date_1 = {"2023", "10", "13"};
+        String[] date_2 = {"2023", "12", "13"};
+        String[] date_3 = {"2024", "10", "13"};
+        String[] result = new String[3];
+        result = collectPersonalInformation.findBigDate(date_1, date_2);
+        System.out.println(Arrays.toString(result));
+        assertThat(result).isEqualTo(date_2);
+    }
+
+    @DisplayName("hash 테스트")
+    @Test
+    void hashTest() {
+        String[] str = {"A 6", "B 12", "C 3"};
+        Map<String, Integer> hasMap = collectPersonalInformation.makeMap(str);
+        System.out.println(hasMap);
+        assertThat(hasMap.get("A")).isEqualTo(6);
+    }
+
+    @DisplayName("날짜 더하기 테스트")
+    @Test
+    void addDateTest() {
+        String date = "2022.12.13";
+        int month = 36;
+        String[] result = collectPersonalInformation.addDate(date, month);
+        System.out.println(Arrays.toString(result));
+        assertThat(result).isNotNull();
+    }
+
+    @DisplayName("개인정보 수집 유효기간 테스트")
+    @Test
+    void mainTest() {
+        String today = "2022.05.19";
+        String[] terms = {"A 6", "B 12", "C 3"};
+        String[] privacies = {"2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"};
+        int[] answer = collectPersonalInformation.solution(today, terms, privacies);
+        System.out.println(Arrays.toString(answer));
+        int[] result = {1, 3};
+        assertThat(answer).isEqualTo(result);
+    }
 }
