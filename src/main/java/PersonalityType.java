58667608fff63ad72c9861c144537231bb78ee10
Kyumin-Chang <kyumin0529@naver.com>
2023-11-25 23:21:55 +0900

    feat: Add PersonalityType Problem

---
+++ src/main/java/PersonalityType.java
@@ -0,0 +1,13 @@
+public class PersonalityType {
+    /*
+    ["AN", "CF", "MJ", "RT", "NA"]	[5, 3, 2, 7, 5]	"TCMA"
+    ["TR", "RT", "TR"]	[7, 1, 3]	"RCJA"
+     */
+
+    public String solution(String[] survey, int[] choices) {
+        String answer = "";
+        return answer;
+    }
+
+
+}
